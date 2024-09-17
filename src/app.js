0738860c6bcbc599aaf5bfeb0c2d5b97f78db806
SilentCodeSage <nandunandakishor345@gmail.com>
2024-09-17 15:41:31 +0530

    initial setup

---
+++ src/app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const app = express();
+
+app.use((req,res)=>{
+    res.send("Hello From The Server")
+})
+
+app.listen(3000,()=>{
+    console.log("Server @ port 3000")
+})
