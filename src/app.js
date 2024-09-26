e92367ea05589264ec89d5d29d38937a5c409bcd
SilentCodeSage <nandunandakishor345@gmail.com>
2024-09-26 19:19:14 +0530

    route methods setup amd initialize

--- src/app.js
+++ src/app.js
@@ -3,20 +3,66 @@ const app = express();
 const connectDB = require("./config/database");
 const User = require("./models/user");
 
+app.use(express.json());
+
+//user signup
 app.post("/signup", async (req, res) => {
   //creating a new instance of a the UserModel
-  const user = new User({
-    firstName: "Nandakishor",
-    lastName: "A S",
-    emailId: "nandunandakishor345@gmail.com",
-    password: "Nandu@123",
-  });
+  const user = new User(req.body);
 
   try {
     await user.save();
     res.send("User info added succesfully");
   } catch (error) {
     res.status(400).send("Error saving the instance");
+  }
+});
+
+//view specific user info
+app.get("/user", async (req, res) => {
+  const userEmail = req.body.emailId;
+  try {
+    const currentUser = await User.findOne({ emailId: userEmail });
+    if (!currentUser) {
+      res.status(400).send("Error fetching the user info");
+    } else {
+      res.send(currentUser);
+    }
+  } catch (error) {
+    res.status(400).send("Error fetching the user info");
+  }
+});
+
+//view all users feed
+app.get("/feed", async (req, res) => {
+  try {
+    const usersList = await User.find({});
+    res.send(usersList);
+  } catch (error) {
+    res.status(400).send("Error fetching the user info" + error);
+  }
+});
+
+//delelte user by the user id
+app.delete("/user", async (req, res) => {
+  try {
+    const user = await User.findByIdAndDelete(req.body.id);
+    res.send("User deleted succesfully");
+  } catch (error) {
+    res.status(400).send("User Deletion failed !" + error);
+  }
+});
+
+//update user LastName
+app.patch("/user", async (req, res) => {
+  try {
+    const usersList = await User.updateOne({
+      firstName: "Nandakishor",
+      lastName: "A.S",
+    });
+    res.send(usersList);
+  } catch (error) {
+    res.status(400).send("Error fetching the user info" + error);
   }
 });
 
