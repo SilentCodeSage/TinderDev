f1488484227e83bf6d2174b46f847da853de97e3
SilentCodeSage <nandunandakishor345@gmail.com>
2024-10-08 10:59:43 +0530

    implemented feed API

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const { userAuth } = require("../middlewares/auth");
 const ConnectionRequestModel = require("../models/connectionRequest");
+const User = require("../models/user");
 const userRouter = express.Router();
 
 //get the pending connection requests for the logged in user.
@@ -50,6 +51,8 @@ userRouter.get("/user/connections", userAuth, async (req, res) => {
         "age",
       ]);
 
+    console.log(connectionRequests);
+
     const data = connectionRequests.map((row) => {
       if (row.fromUserId._id.toString() === currentUser._id.toString()) {
         return row.toUserId;
@@ -57,6 +60,40 @@ userRouter.get("/user/connections", userAuth, async (req, res) => {
       return row.fromUserId;
     });
     res.send(data);
+  } catch (error) {
+    res.send("Error: " + error.message);
+  }
+});
+
+userRouter.get("/feed", userAuth, async (req, res) => {
+  try {
+    const currentUser = req.currentUser;
+
+    const usersToHideFromfeedInfo = await ConnectionRequestModel.find({
+      $or: [{ toUserId: currentUser._id }, { fromUserId: currentUser._id }],
+    });
+    const usersToHideFromfeed = new Set();
+
+    usersToHideFromfeedInfo.forEach((data) => {
+      usersToHideFromfeed.add(data.fromUserId.toString());
+      usersToHideFromfeed.add(data.toUserId.toString());
+    });
+
+    const usersToShowInFeed = await User.find({
+      $and: [
+        { _id: { $nin: usersToHideFromfeed } },
+        { _id: { $ne: currentUser._id } },
+      ],
+    }).select([
+      "firstName",
+      "lastName",
+      "profileUrl",
+      "about",
+      "gender",
+      "age",
+    ]);
+    console.log(usersToHideFromfeed);
+    res.send(usersToHideFromfeed);
   } catch (error) {
     res.send("Error: " + error.message);
   }
